87555060db9c05ecc97e085ad7469f292ddccd9d
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2021-02-08 00:04:44 +0300

    feat: login

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,8 @@
 import { Request, Response, Router } from "express";
-import { validate } from "class-validator";
+import { validate, isEmpty } from "class-validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cookie from "cookie";
 
 import { User } from "../entities/User";
 
@@ -38,7 +41,79 @@ const register = async (req: Request, res: Response) => {
   }
 };
 
+const login = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  try {
+    let errors: any = {};
+
+    if (isEmpty(username)) errors.username = "Username must not be empty";
+    if (isEmpty(password)) errors.password = "Password must not be empty";
+
+    if (Object.keys(errors).length) {
+      return res.status(400).json(errors);
+    }
+    const user = await User.findOne({ username });
+
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    const passwordMatches = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatches) {
+      return res.status(401).json({ error: "Wrong Credentials" });
+    }
+
+    const token = jwt.sign({ username }, process.env.JWT_SECRET);
+
+    res.set(
+      "Set-Cookie",
+      cookie.serialize("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict", // cookie should only come from our domain
+        maxAge: 3600,
+        path: "/", // cookie will be valid  in every app path
+      })
+    );
+    return res.json({ user });
+  } catch (err) {}
+};
+
+// traditionally called me
+const me = async (req: Request, res: Response) => {
+  try {
+    const token = req.cookies.token; // by cookie-parser
+    if (!token) throw new Error("Unauthenticated");
+
+    const { username }: any = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findOne({ username });
+
+    if (!username) throw new Error("Unauthenticated");
+    return res.json(user);
+  } catch (err) {
+    return res.status(401).json({ error: err.message });
+  }
+};
+
+const logout = (_: Request, res: Response) => {
+  res.set(
+    "Set-Cookie",
+    cookie.serialize("token", "", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict", // cookie should only come from our domain
+      expires: new Date(0),
+      path: "/",
+    })
+  );
+
+  return res.status(200).json({ sucess: true });
+};
+
 const router = Router();
 router.post("/register", register);
+router.post("/login", login);
+router.get("/me", me);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -3,16 +3,19 @@ import { createConnection } from "typeorm";
 import chalk from "chalk";
 import express from "express";
 import morgan from "morgan";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
-import { User } from "./entities/User";
 import authRoutes from "./routes/auth";
 import trim from "./middleware/trim";
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(trim);
+app.use(cookieParser());
 
 app.get("/", (req, res) => res.send("hello "));
 app.use("/api/auth", authRoutes);
