dde64b406eaa23e822d5c9fa66c99f1cfb7e4d8f
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2021-02-13 14:32:33 +0300

    feat: split middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -3,16 +3,11 @@ import jwt from "jsonwebtoken";
 
 import User from "../entities/User";
 
-export default async (req: Request, res: Response, next: NextFunction) => {
+export default async (_: Request, res: Response, next: NextFunction) => {
   try {
-    const token = req.cookies.token; // cookie parsed by cookie-parser
-    if (!token) throw new Error("Unauthenticated");
+    const user: User | undefined = res.locals.user;
 
-    const { username }: any = jwt.verify(token, process.env.JWT_SECRET!);
-
-    const user = await User.findOne({ username });
-
-    if (!username) throw new Error("Unauthenticated");
+    if (!user) throw new Error("Unauthenticated");
 
     res.locals.user = user;
     return next();
--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,16 +1,17 @@
 import { Router, Request, Response } from "express";
 import Post from "../entities/Post";
 
-
-import auth from "../middleware/auth";
 import Sub from "../entities/Sub";
 import Comment from "../entities/Comment";
+
+import auth from "../middleware/auth";
+import user from "../middleware/user";
 
 const createPost = async (req: Request, res: Response) => {
   const { title, body, sub } = req.body;
 
   const user = res.locals.user;
-  console.log("user here => ", user)
+  console.log("user here => ", user);
 
   if (title.trim() === "") {
     return res.status(400).json({
@@ -21,7 +22,7 @@ const createPost = async (req: Request, res: Response) => {
   try {
     //   find sub
 
-    const subRecord = await Sub.findOneOrFail({ name: sub })
+    const subRecord = await Sub.findOneOrFail({ name: sub });
 
     const post = new Post({ title, body, user, sub: subRecord });
 
@@ -39,57 +40,61 @@ const createPost = async (req: Request, res: Response) => {
 const getPosts = async (_: Request, res: Response) => {
   try {
     const posts = await Post.find({
-      order: { createdAt: 'DESC' },
-      // relations: ['sub',]
-    })
-    return res.json(posts)
+      order: { createdAt: "DESC" },
+      relations: ["sub", "comments", "votes"],
+    });
+    if (res.locals.user) {
+      posts.forEach((p) => p.setUserVote(res.locals.user));
+    }
+    return res.json(posts);
   } catch (err) {
-    console.log(err)
-    return res.status(500).json({ error: 'Something went wrong' })
+    console.log(err);
+    return res.status(500).json({ error: "Something went wrong" });
   }
-}
-
+};
 
 const getPost = async (req: Request, res: Response) => {
   const { identitfier, slug } = req.params;
 
   try {
-    const post = await Post.findOneOrFail({ identitfier, slug }, { relations: ['sub'] })
-    return res.json(post)
+    const post = await Post.findOneOrFail(
+      { identitfier, slug },
+      { relations: ["sub"] }
+    );
+    return res.json(post);
   } catch (err) {
-    console.log(err)
-    return res.status(404).json({ error: 'Post not found' })
+    console.log(err);
+    return res.status(404).json({ error: "Post not found" });
   }
-}
+};
 
 const commentOnPost = async (req: Request, res: Response) => {
   const { identitfier, slug } = req.params;
-  const body = req.body.body
+  const body = req.body.body;
 
   try {
-    const post = await Post.findOneOrFail({ identitfier, slug })
+    const post = await Post.findOneOrFail({ identitfier, slug });
 
     const comment = new Comment({
       body,
       user: res.locals.user,
       post,
-    })
+    });
 
-    await comment.save()
+    await comment.save();
 
-    return res.json(comment)
+    return res.json(comment);
   } catch (err) {
-    console.log(err)
-    return res.status(404).json({ error: 'Post not found' })
-
+    console.log(err);
+    return res.status(404).json({ error: "Post not found" });
   }
-}
+};
 
 const router = Router();
 
-router.post("/", auth, createPost);
-router.get('/', getPosts);
-router.get('/:identitfier/:slug', getPost)
-router.post('/:identitfier/:slug/comments', auth, commentOnPost)
+router.post("/", user, auth, createPost);
+router.get("/", user, getPosts);
+router.get("/:identitfier/:slug", getPost);
+router.post("/:identitfier/:slug/comments", user, auth, commentOnPost);
 
 export default router;
