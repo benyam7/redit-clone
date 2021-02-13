5771a7bac4f7cef6a620e8b6a0aa741bf4016029
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2021-02-13 19:15:08 +0300

    feat: upload image

--- src/routes/sub.ts
+++ src/routes/sub.ts
@@ -1,12 +1,15 @@
-import { Request, Response, Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { isEmpty } from "class-validator";
 import { getRepository } from "typeorm";
+import multer, { FileFilterCallback } from "multer";
+import path from "path";
 
 import User from "../entities/User";
 import auth from "../middleware/auth";
 import Sub from "../entities/Sub";
 import user from "../middleware/user";
 import Post from "../entities/Post";
+import { makeid } from "../utils/util";
 
 const createSub = async (req: Request, res: Response) => {
   const { name, title, description } = req.body;
@@ -63,12 +66,50 @@ const getSub = async (req: Request, res: Response) => {
     return res.json(sub);
   } catch (err) {
     console.log(err);
-    return res.status(500).json({ error: "Something went wrong" });
+    return res.status(404).json({ sub: "Sub not found" });
   }
+};
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: "public/images",
+    filename: (_, file, callback) => {
+      const name = makeid(15);
+      callback(null, name + path.extname(file.originalname)); // e.g. jh34gh2v4y + .png
+    },
+  }),
+  fileFilter: (_, file: any, callback: FileFilterCallback) => {
+    if (file.mimetype == "image/jpeg" || file.mimetype == "image/png") {
+      callback(null, true);
+    } else {
+      callback(new Error("Not an image"));
+    }
+  },
+});
+
+const ownSub = async (req: Request, res: Response, next: NextFunction) => {
+  const user: User = res.locals.user;
+  try {
+    const sub = await Sub.findOneOrFail({
+      where: { name: req.params.name },
+    });
+  } catch (err) {}
+};
+
+const uploadSubImage = async (_: Request, res: Response) => {
+  return res.json({ success: true });
 };
 
 const router = Router();
 
 router.post("/", user, auth, createSub);
 router.get("/:name", user, getSub);
+router.post(
+  "/:name/image",
+  user,
+  auth,
+  ownSub,
+  upload.single("file"),
+  uploadSubImage
+);
 export default router;
